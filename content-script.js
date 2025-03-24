4e0dcaf8027d958dfad0569138a6091c9d3c78cf
Nandisha <197202821+nandisha0@users.noreply.github.com>
2025-03-24 20:16:49 +0530

    add chrome extension files

---
+++ content-script.js
@@ -0,0 +1,56 @@
+console.log('Hi from Grafana extension');
+
+function selectSystem(hostname) {
+    console.log(hostname);
+    const systemSelector = document.getElementsByClassName('css-10l6kcd')[0];
+    if (!systemSelector) {
+        console.log('Web page is not loaded yet');
+        return;
+    }
+    systemSelector.click();
+
+    const anchors = document.querySelectorAll('a.variable-option');
+    const targetAnchor = Array.from(anchors).find((anchor) =>
+        anchor.textContent.includes(hostname),
+    );
+    targetAnchor.click();
+}
+
+function rotate(hosts, index = 0) {
+    setTimeout(() => rotate(hosts, newIndex), time);
+    selectSystem(hosts[index]);
+    const newIndex = (index + 1) % hosts.length;
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+const systemMap = {
+    m1: 'IFIYCLSDT033',
+    m2: 'IFIYCLSDT002',
+    m3: 'IFIYCLSDT007',
+    m4: 'IFIYCLSDT036',
+    m5: 'IFIYCLSDT030',
+    m6: 'IFIYCLSDT005',
+    m7: 'IFIYCLSDT029',
+    m8: 'IFIYCLSDT016',
+    m9: 'IFIYCLSDT0015',
+    m10: 'IFIYCLSDT034',
+    m11: 'IFIYCLSDT035',
+    m12: 'IFIYCLSDT024',
+    b1: 'IFIYCLSDT019',
+    b2: 'IFIYCLSDT010',
+    b3: 'IFIYCLSDT014',
+    b4: 'IFIYCLSDT037',
+    b5: 'IFIYCLSDT031',
+    b6: 'IFIYCLSDT017',
+    b7: 'IFIYCLSDT008',
+    b8: 'IFIYCLSDT003',
+    b9: 'IFIYCLSDT004',
+    b10: 'IFIYCLSDT018',
+    b11: 'IFIYCLSDT022',
+    b12: 'IFIYCLSDT009',
+};
+
+let time = 5000;
+const systems = ['m1', 'm2', 'm3', 'm4', 'm5'];
+const hosts = systems.map((id) => systemMap[id]);
+rotate(hosts);
